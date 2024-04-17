d187de21abed8e68bd5c73812a10019d9fc1c11a
macosx0 <marcosagnus4321@gmail.com>
2024-04-16 21:09:37 -0300

    atualização

--- src/componentes/note-card.tsx
+++ src/componentes/note-card.tsx
@@ -25,8 +25,8 @@ export function Notecard({ note }: NoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
         <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
-          <Dialog.Close className="">
-            <X />
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
           </Dialog.Close>
           <div className="flex flex-1 flex-col gap-5 p-5">
             <span className=" text-sm font-medium text-slate-300">
@@ -41,8 +41,8 @@ export function Notecard({ note }: NoteCardProps) {
             type="button"
             className="w=full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
-            Deseja
-            <span className="text-red-400 group-hover:underline ">
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
               apagar essa nota?
             </span>
           </button>
