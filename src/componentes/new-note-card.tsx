01bec505ccd0f445137ad580a518be2f579c4a81
macosx0 <marcosagnus4321@gmail.com>
2024-04-23 10:25:11 -0300

    atualizacao

--- src/componentes/new-note-card.tsx
+++ src/componentes/new-note-card.tsx
@@ -1,10 +1,48 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "lucide-react";
+import * as Dialog from '@radix-ui/react-dialog';
+import { X } from 'lucide-react';
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from 'sonner'
 
-export function NewnoteCard() {
+interface NewNoteCardProps{
+  onNoteCreated: (content: string)=>void
+}
+
+export function NewnoteCard({ onNoteCreated }: NewNoteCardProps) {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+  const [isRecording, setIsRecording] = useState(false)
+  const [content, setContent] = useState('')
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false) 
+  }
+
+  function handleContetChanged(event:ChangeEvent<HTMLTextAreaElement> ) {
+   setContent(event.target.value)
+
+    if (event.target.value === '') {
+      setShouldShowOnboarding(true)
+    }
+  }
+
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault()
+
+    onNoteCreated (content)
+
+    setContent('')
+
+    setShouldShowOnboarding(true)
+
+    toast.success('Nota criada com Sucesso')
+  }
+
+  function handleStartRecording() {
+     setIsRecording(true)
+  }
+
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 space-y-3">
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none  hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className=" text-sm font-medium text-slate-200">
           Adicionar Nota
         </span>
@@ -20,19 +58,44 @@ export function NewnoteCard() {
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
+          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
           <div className="flex flex-1 flex-col gap-5 p-5">
-            <span className=" text-sm font-medium text-slate-300"></span>
-            <p className="text-sm leading-6 text-slate-400">{}</p>
-          </div>
-          <button
-            type="button"
-            className="w=full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
-          >
-            Deseja{" "}
-            <span className="text-red-400 group-hover:underline">
-              apagar essa nota?
+            <span className=" text-sm font-medium text-slate-300">
+              Adicionar nota
             </span>
-          </button>
+
+            {shouldShowOnboarding ? (
+              <p className="text-sm leading-6 text-slate-400">Comece <button type='button' onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline"> gravando uma nota </button> em áudio ou se preferir <button type='button' onClick={handleStartEditor}className="font-medium text-lime-400 hover:underline"> utilize apenas texto </button>
+            </p>
+            ) : (
+              <textarea 
+              autoFocus
+              className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+              onChange={handleContetChanged}
+              value={content}
+
+              />
+            )}
+          </div>
+          
+          {isRecording ? (
+          <button
+          type="submit"
+            className="w=full bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+          >
+            Gravando! (clique p/ interromper)
+          </button> 
+          ) : (
+             
+          <button
+          type="submit"
+          className="w=full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+        >
+          Salvar nota
+        </button> 
+          )}
+ 
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
